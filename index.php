f4ab491a72907ff050a15b6050bfbbb1c12af072
Victor Aguilar <valbert1993@gmail.com>
2016-05-07 05:26:29 -0600

    create medical benefits

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include "layouts/header.php";
 
 <a href="contact_information.php" class="btn btn-default">contact information</a>
 <a href="new-client.php" class="btn btn-default">New Client</a>
+<a href="medical-benefits.php" class="btn btn-default">Medical Benefits</a>
 
 
 <?php
---
+++ medical-benefits.php
@@ -0,0 +1,17 @@
+<?php
+include "layouts/header.php";
+?>
+<div class="form-title">
+	<p>Medical benefits</p>
+</div>
+<div class="container main cntact_information">
+	<div class="row">
+		<div class="col-xs-12">
+
+		</div>
+	</div>
+</div>
+<?php
+include "layouts/back-save.php";
+include "layouts/footer.php";
+?>
