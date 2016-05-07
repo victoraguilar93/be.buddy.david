ce99f9fc8ed4d7087ebc1f5e7f76a7e81a3f6e88
Victor Aguilar <valbert1993@gmail.com>
2016-05-07 06:26:17 -0600

    medical 70%

--- medical-benefits.php
+++ medical-benefits.php
@@ -4,10 +4,27 @@ include "layouts/header.php";
 <div class="form-title">
 	<p>Medical benefits</p>
 </div>
-<div class="container main cntact_information">
-	<div class="row">
+<div class="container main medical-benefits">
+	<div class="row resourses-title">
 		<div class="col-xs-12">
-
+				<p>Resource assets</p>
+				<br>
+				<div class="hr"></div>
+		</div>
+	</div>
+	<div class="row summary">
+		<div class="col-xs-2 file-title">Summary name:</div>
+		<div class="col-xs-8 name"><p></p></div>
+		<div class="col-xs-2 button-upload"><button type="button" class="btn"><span class="glyphicon glyphicon-open" aria-hidden="true"></span>&nbsp;&nbsp;Upload File</button></div>
+	</div>
+	<div class="row summary">
+		<div class="col-xs-2 file-title">Summary name:</div>
+		<div class="col-xs-8 name"><p></p></div>
+		<div class="col-xs-2 button-upload"><button type="button" class="btn"><span class="glyphicon glyphicon-open" aria-hidden="true"></span>&nbsp;&nbsp;Upload File</button></div>
+	</div>
+	<div class="row button-add">
+		<div class="col-xs-12">
+			<button type="button" class="btn"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>&nbsp;&nbsp;Add new plan</button>
 		</div>
 	</div>
 </div>
