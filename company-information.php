d1bbbc9c0d3adcdad793c6c035f6497ca1884940
Victor Aguilar <valbert1993@gmail.com>
2016-05-08 10:09:48 -0600

    company information

---
+++ company-information.php
@@ -0,0 +1,100 @@
+<?php
+include "layouts/header.php";
+?>
+<div class="form-title">
+	<p>Custom home screen message</p>
+</div>
+<div class="container main company-information">
+		<div class="row page-title">
+			<div class="col-xs-12">
+					<div class="col-xs-3 col-xs-offset-1 company-name">
+						<label for="company-name">New client company name:</label>
+					</div>
+					<div class="col-xs-5 input-company-name">
+						<input type="text" placeholder="Lorem ipsum">
+					</div>
+					<br>
+					<div class="col-xs-12 hr"></div>
+			</div>
+		</div>
+		<div class="row plans">
+			<p class="title-form">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum aliquam qui saepe dolor nobis ea nostrum modi neque, natus animi adipisci officia eveniet tempora inventore.</p>
+			<div class="col-xs-12 col-sm-6">
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan1" type="checkbox">
+					<label for="check-plan1"><span></span>Medical</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan2" type="checkbox">
+					<label for="check-plan2"><span></span>Dental</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan3" type="checkbox">
+					<label for="check-plan3"><span></span>Vision</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan4" type="checkbox">
+					<label for="check-plan4"><span></span>HSA (health savings account)</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan5" type="checkbox">
+					<label for="check-plan5"><span></span>HRA (health reimbursment account)</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan6" type="checkbox">
+					<label for="check-plan6"><span></span>FSA (flexible spending account)</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan7" type="checkbox">
+					<label for="check-plan7"><span></span>Life</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan8" type="checkbox">
+					<label for="check-plan8"><span></span>Short-term Disability</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan9" type="checkbox">
+					<label for="check-plan9"><span></span>Long-term Disability</label>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6">
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan11" type="checkbox">
+					<label for="check-plan11"><span></span>EAP (employee assistant program)</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan12" type="checkbox">
+					<label for="check-plan12"><span></span>Accident</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan13" type="checkbox">
+					<label for="check-plan13"><span></span>Cancer</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan14" type="checkbox">
+					<label for="check-plan14"><span></span>Critical Illness</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan15" type="checkbox">
+					<label for="check-plan15"><span></span>401k</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan16" type="checkbox">
+					<label for="check-plan16"><span></span>Telemedicine (formerly Call A Doc)</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan17" type="checkbox">
+					<label for="check-plan17"><span></span>24 Hour Nurse Hotline</label>
+				</div>
+				<div class="col-xs-12 check-plan">
+					<input id="check-plan18" type="checkbox">
+					<label for="check-plan18"><span></span>Rx (only if outside of your medical offerings)</label>
+				</div>
+			</div>
+		</div>
+
+
+</div>
+<?php
+include "layouts/footer.php";
+?>
